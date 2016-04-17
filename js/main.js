ef6cbb34713fcb340f69f3c7523225868e5c4c27
Dan McCurley <djmccurley@gmail.com>
2016-04-17 14:39:05 -0500

    js complete!

--- js/main.js
+++ js/main.js
@@ -37,3 +37,74 @@ navButton3.addEventListener('click', function() {
     .classList
     .toggle('on-click');
 });
+
+var navButton4 = document.querySelector('.fa-paw');
+
+navButton4.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-paw')
+    .classList
+    .toggle('on-click');
+});
+
+var navButton5 = document.querySelector('.fa-plug');
+
+navButton5.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-plug')
+    .classList
+    .toggle('on-click');
+});
+
+var navButton6 = document.querySelector('.fa-futbol-o');
+
+navButton6.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-futbol-o')
+    .classList
+    .toggle('on-click');
+});
+
+var navButton7 = document.querySelector('.fa-tree');
+
+navButton7.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-tree')
+    .classList
+    .toggle('on-click');
+});
+
+var navButton8 = document.querySelector('.fa-wheelchair');
+
+navButton8.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-wheelchair')
+    .classList
+    .toggle('on-click');
+});
+
+var navButton9 = document.querySelector('.fa-bicycle');
+
+navButton9.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-bicycle')
+    .classList
+    .toggle('on-click');
+});
+
+var navButton10 = document.querySelector('.fa-heart');
+
+navButton10.addEventListener('click', function() {
+  console.log('amenity click!');
+  document
+    .querySelector('.fa-heart')
+    .classList
+    .toggle('on-click');
+});
+
